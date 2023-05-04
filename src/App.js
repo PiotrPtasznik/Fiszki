cfcda461fc419668f4f2fd60ffcc04975ae74b83
PiotrPtasznik <piotr.ptasznik111@gmail.com>
2023-05-04 02:14:11 +0200

    React added, api-frontend connnection configured

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useEffect, useState } from "react";
+import Flashcard from "./Flashcard"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    data:[]
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:8080/flashcards')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({data})
+    }
+    );
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.data.map(flashcard => <Flashcard info={flashcard}/>)}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
