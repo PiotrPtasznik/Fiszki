8efecfabb9fce64c0ae710d0fa1a036ac21ae04c
PiotrPtasznik <piotr.ptasznik111@gmail.com>
2023-05-10 00:41:15 +0200

    Scrolling throught flashcards added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,65 @@
-import React, { Component, useEffect, useState } from "react";
-import Flashcard from "./Flashcard"
+import React, { Component, useState, useEffect } from "react";
+import Flashcard from "./components/Flashcard"
 
-class App extends Component{
-  state = {
-    data:[]
+
+const App = () => {
+
+  const [data, setData] = useState([])
+
+  const [flashcard, setFlashcard] = useState({
+    frontside: "default frontside",
+    backside: "default backside"
+  });
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const setFlashcardData = (frontsideData, backsideData) => {
+    setFlashcard(() => {
+      return {
+        frontside: frontsideData,
+        backside: backsideData
+      }
+    });
   }
 
-  componentDidMount(){
-    fetch('http://localhost:8080/flashcards')
-    .then(response => response.json())
-    .then(data => {
-      this.setState({data})
-    }
-    );
+  const fetchData = async () => {
+    const response = await fetch('http://localhost:8080/flashcards');
+    const data = await response.json();
+    setData(data);
+    setFlashcardData(
+      data[0].frontside,
+      data[0].backside)
   }
 
-  render(){
-    return(
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleNextButton = () => {
+    setCurrentIndex(currentIndex + 1);
+    setFlashcardData(
+      data[currentIndex].frontside,
+      data[currentIndex].backside)
+  };
+
+  const handlePreviousButton = () => {
+    setCurrentIndex(currentIndex - 1);
+    setFlashcardData(
+      data[currentIndex].frontside,
+      data[currentIndex].backside)
+  };
+
+  return (
+    <div>
       <div>
-        {this.state.data.map(flashcard => <Flashcard info={flashcard}/>)}
+        <p>Fiszka : {flashcard.frontside}|{flashcard.backside} {currentIndex}</p>
+        
       </div>
-    );
-  }
+      <button onClick={handlePreviousButton}>Previous</button>
+      <button onClick={handleNextButton}>Next</button>
+    </div>
+  );
+
 }
 
 export default App;
